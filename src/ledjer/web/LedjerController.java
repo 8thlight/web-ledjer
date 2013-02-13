9e9a685912045fa0531c7aa7d3aec91ec6a93ab2
Micah <micahmartin@gmail.com>
2013-02-13 11:22:31 +0000

    can submit deposits

--- src/ledjer/web/LedjerController.java
+++ src/ledjer/web/LedjerController.java
@@ -1,10 +1,16 @@
 package ledjer.web;
 
+import ledjer.Deposit;
 import ledjer.Ledger;
+import ledjer.Payment;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.util.Date;
 import java.util.Map;
 
 @Controller
@@ -18,12 +24,25 @@ public class LedjerController
     this.ledger = ledger;
   }
 
-  @RequestMapping({"/", "/index"})
+  @RequestMapping(value = "/", method = RequestMethod.GET)
   public String showDefault(Map<String, Object> model)
   {
+    System.out.println("model.size() = " + model.size());
     for(String s : model.keySet())
       System.out.println(s + " => " + model.get(s));
     model.put("ledger", ledger);
+    model.put("deposit", new Deposit(0, new Date()));
+    model.put("payment", new Payment(0, new Date(), ""));
     return "index";
+  }
+
+  @RequestMapping(value = "/deposit", method = RequestMethod.POST)
+  public String deposit(Deposit deposit, BindingResult bindingResult)
+  {
+    System.out.println("DEPOSIT!!!!!");
+    System.out.println("deposit = " + deposit);
+    System.out.println("deposit.getNumber() = " + deposit.getNumber());
+    System.out.println("deposit.getAmount() = " + deposit.getAmount());
+    return "redirect:/";
   }
 }
--- src/main/webapp/WEB-INF/views/index.jsp
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -2,6 +2,26 @@
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %>
 <%@ taglib prefix="s" uri="http://www.springframework.org/tags"%>
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form"%>
 
 <h1>HEllo</h1>
 <h2>${ledger.getClass()}</h2>
+
+
+<form:form method="POST" action="/deposit" modelAttribute="deposit">
+    <fieldset>
+        <label for="deposit_amount">Amount</label>
+        <form:input path="amount" size="5" id="deposit_amount"/>
+        <input type="submit" value="Enter Deposit"/>
+    </fieldset>
+</form:form>
+
+<form:form method="POST" action="pay" modelAttribute="payment">
+    <fieldset>
+        <label for="payment_amount">Amount</label>
+        <form:input path="amount" size="5" id="payment_amount"/>
+        <label for="payment_payee">Payee</label>
+        <form:input path="payee" size="20" id="payment_payee"/>
+        <input type="submit" value="Enter Payment"/>
+    </fieldset>
+</form:form>
