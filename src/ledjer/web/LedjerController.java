f8c8e1491570bc39099dab38c7f5aadc9b72b421
Micah <micahmartin@gmail.com>
2013-02-13 08:25:46 +0000

    JSP displaying dynamic content

--- src/ledjer/web/LedjerController.java
+++ src/ledjer/web/LedjerController.java
@@ -1,18 +1,29 @@
 package ledjer.web;
 
+import ledjer.Ledger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Map;
 
 @Controller
-public class DefaultController
+public class LedjerController
 {
+  private final Ledger ledger;
+
+  @Autowired
+  public LedjerController(Ledger ledger)
+  {
+    this.ledger = ledger;
+  }
+
   @RequestMapping({"/", "/index"})
   public String showDefault(Map<String, Object> model)
   {
     for(String s : model.keySet())
       System.out.println(s + " => " + model.get(s));
+    model.put("ledger", ledger);
     return "index";
   }
 }
---
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,7 @@
+<%@ page isELIgnored="false" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %>
+<%@ taglib prefix="s" uri="http://www.springframework.org/tags"%>
+
+<h1>HEllo</h1>
+<h2>${ledger.getClass()}</h2>
